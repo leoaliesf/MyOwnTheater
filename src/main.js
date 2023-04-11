caa4034b74b3185a101b7bf5301716aed2f3a8d9
leoaliesf <leoaliesf@gmail.com>
2023-04-11 22:17:21 +0200

    haciendo get trending

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 
+const getTrending = async function () {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+ apiKey)
+    const data = await res.json();
+    const movies = data.results;
+    movies.forEach(movie=> {
+        const trendingPreviewC = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300'+ movie.poster_path)    
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewC.appendChild(movieContainer);
+
+    });
+}
+getTrending();
